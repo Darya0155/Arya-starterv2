15cf21ff822456f2a6d7e8c18d119c0e487b43b7
Deepak <daryabusiness@gmail.com>
2024-07-11 20:57:35 +0530

    fixing something

--- arya-core-starter/src/main/java/com/arya/pmd/DuplicateTest.java
+++ arya-core-starter/src/main/java/com/arya/pmd/DuplicateTest.java
@@ -6,9 +6,14 @@ public class DuplicateTest {
 
     public void f1(){
 
-
+        String s = "" + 123;
+        System.out.println(s);
+        System.out.println("PrintHelloworld");
+        System.out.println("PrintHelloworld");
     }
     public void f2(){
+        System.out.println("PrintHelloworld");
+        System.out.println("PrintHelloworld");
     }
 
 }
